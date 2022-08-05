b018805ad5c6bcf81d41a4bfebdbd240921b2dbc
Greta Kabanovaite <gxk912@cs.bham.ac.uk>
2022-08-05 13:58:57 +0100

    Exercises: Practise Programming #1, #2 and #3.

---
+++ src/main/java/com/sg/foundations/variables/BestAdderEver.java
@@ -0,0 +1,12 @@
+package com.sg.foundations.variables;
+
+public class BestAdderEver {
+    public static void main (String[] args){
+        int a, b, c;
+        a=5;
+        b=2;
+        c=1;
+        int sum = a+b+c;
+        System.out.println("Adding: " + a + " + " + b + " + " + c+ " = " + sum);
+    }
+}
